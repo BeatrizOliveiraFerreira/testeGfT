daf37f01b7dece679e6e7e908b2e875fb3609c20
Beatriz Oliveira Ferreira <66791075+BeatrizOliveiraFerreira@users.noreply.github.com>
2021-09-16 17:37:21 -0300

    numeros

---
+++ index.js
@@ -0,0 +1,7 @@
+const cincoNumeros = [1,2,3,4,5]
+const total = cincoNumeros.reduce(
+  (total, currentElement) => total + currentElement
+)
+
+console.log(`São ${cincoNumeros[0], cincoNumeros[1], cincoNumeros[2], cincoNumeros[3], cincoNumeros}`)
+console.log(`O total é: ${total} reais`)
